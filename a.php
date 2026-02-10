f2797f6ab165d5e6e206618147c304e98cfdbe1c
66010914087-wq <66010914087@msu.ac.th>
2026-02-10 09:55:14 +0700

    i

---
+++ a.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อมรรัตน์ ทองอินทา(หมิว)</title>
+</head>
+
+<body>
+
+<h1>งาน i </h1>
+<h1>อมรรัตน์ ทองอินทา(หมิว)</h1>
+
+
+</body>
+</html>
